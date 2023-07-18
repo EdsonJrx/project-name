2339cfd59702e146554ead792bb927b191de03db
EdsonJrx <edinhoa7x@gmail.com>
2023-07-18 18:19:22 -0400

    Learning Nest

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CatsController } from './cats/cats.controller';
+import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [CatsModule],
+  controllers: [CatsController],
+  providers: [ CatsService],
+})
+export class AppModule {}
